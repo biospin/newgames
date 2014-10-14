d2ea74e894282bf6ef719f5749050754819902b6
moo.biocloud <lab.moo@gmail.com>
2014-10-14 10:56:06 +0900

    [fix] toJSON 함수 고침

--- apiSrv/student/views.py
+++ apiSrv/student/views.py
@@ -5,18 +5,19 @@ import json
 
 # Create your views here.
 
-def toJSON(objs, status=200) :
-   j = json.dumps(objs, ensure_ascii=False)
-   return HttpResponse(j, status=status, content_type='applcation/json; charset=utf-8')
+def toJSON(objs, status=200):
+    j = json.dumps(objs, ensure_ascii=False)
+    return HttpResponse(j, status=status, content_type='application/json; charset=utf-8')
+
 
 def student_view(request) :
     rst = {}
     rst['msg'] = "hello"
 
-    return toJSON(rst)
+    return toJSON(rst, 200)
 
 def student_detail_view(request, id) :
     rst = {}
     rst['msg'] = "hello"
 
-    return toJSON(rst)
+    return toJSON(rst, 200)
