8f723e1d7627a3e92d684a5f2721865642113ff1
moo.biocloud <lab.moo@gmail.com>
2014-10-14 18:06:52 +0900

    [step.0] student 앱 생성 후 셋팅

--- apiSrv/student/views.py
+++ apiSrv/student/views.py
@@ -1,23 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-import json
 
 # Create your views here.
-
-def toJSON(objs, status=200):
-    j = json.dumps(objs, ensure_ascii=False)
-    return HttpResponse(j, status=status, content_type='application/json; charset=utf-8')
-
-
-def student_view(request) :
-    rst = {}
-    rst['msg'] = "hello"
-
-    return toJSON(rst, 200)
-
-def student_detail_view(request, id) :
-    rst = {}
-    rst['msg'] = "hello"
-
-    return toJSON(rst, 200)
